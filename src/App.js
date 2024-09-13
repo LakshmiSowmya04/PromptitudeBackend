efaefa8e948485a6d18d498acb47e38ef266dcb9
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2024-09-13 19:36:50 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/NavBar";
+import Body from "./components/Body";
+import MyAccount from "./components/MyAccount";
+import Reminder from "./components/Remainder";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Nav />
+        <Body>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/my-account" element={<MyAccount />} />
+              <Route path="/reminder" element={<Reminder />} />
+            </Routes>
+          </Router>
+        </Body>
+      </div>
     </div>
   );
 }
